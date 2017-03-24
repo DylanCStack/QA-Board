fba53ae78b4d8a54aa90a8a21a1d265bcd94cc27
Dylan Stackhouse <stackhouse.dylan@gmail.com>
2017-03-24 09:14:31 -0700

    add saveQuestion functionality to new-question and index route

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -3,11 +3,21 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     addNewQuestion: false,
-    questionFormShow(){
+    questionFormShow() {
       this.set("addNewQuestion", true);
     },
-    questionFormHide(){
+    questionFormHide() {
       this.set("addNewQuestion", false);
+    },
+    saveQuestion() {
+      var params = {
+        author: this.get("author"),
+        subject: this.get("subject"),
+        question: this.get("question"),
+        body: this.get("body")
+      };
+      this.set("addNewQuestion", false);
+      this.sendAction("saveQuestion", params);
     }
   }
 });
