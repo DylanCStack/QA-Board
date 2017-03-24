6e7b6c41b5951af5b20f889186c040039e350b99
Dylan Stackhouse <stackhouse.dylan@gmail.com>
2017-03-24 10:31:27 -0700

    add new-answer component with toggleable form to question page

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  actions:{
+    addNewAnswer: false,
+    newAnswerFormShow(){
+      this.set("addNewAnswer", true);
+    },
+    newAnswerFormHide(){
+      this.set("addNewAnswer", false);
+    }
+  }
 });
